2a4ae23a084a8312af14b435ddabb28f58221924
4samad <smd2k15@gmail.com>
2025-03-20 20:43:25 +0530

    :bento: update icons

--- packages/nextjs/utils/scaffold-eth/getMetadata.ts
+++ packages/nextjs/utils/scaffold-eth/getMetadata.ts
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 const baseUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
   : `http://localhost:${process.env.PORT || 3000}`;
-const titleTemplate = "%s | AIMDAO-alpha";
+const titleTemplate = "%s | AIMDAO";
 
 export const getMetadata = ({
   title,
@@ -42,9 +42,6 @@ export const getMetadata = ({
       },
       description: description,
       images: [imageUrl],
-    },
-    icons: {
-      icon: [{ url: "/favicon.png", sizes: "32x32", type: "image/png" }],
     },
   };
 };
